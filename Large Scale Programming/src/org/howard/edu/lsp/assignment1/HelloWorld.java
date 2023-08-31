d7ea74e993fe57f296ef8dfaf916ec57dc1916a6
johanlingani <johanlingani@johans-mbp-286.lan>
2023-08-31 17:05:10 -0400

    Second Commit

--- Large Scale Programming/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ Large Scale Programming/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,10 +1,12 @@
 package org.howard.edu.lsp.assignment1;
 
+//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
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello World");
 	}
 
 }
